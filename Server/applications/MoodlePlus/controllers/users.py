f8ca037c9ebfc2e357ec5def99ba26431455e941
aditi741997 <aditi741997@gmail.com>
2016-02-29 15:03:32 +0530

    Added information about complaints

--- Server/applications/MoodlePlus/controllers/users.py
+++
@@ -1,3 +0,0 @@
-def user():
-	user = db(db.users.id==request.args[0]).select().first()
-	return dict(user=user)
--- Server/applications/MoodlePlus/models/static_vars.py
+++
@@ -1,5 +0,0 @@
-def get_current_year():
-	return 2015 if db(db.static_vars.name=="current_year").count()<1 else db(db.static_vars.name=="current_year").select().first().int_value
-
-def get_current_sem():
-	return 2 if db(db.static_vars.name=="current_sem").count()<1 else db(db.static_vars.name=="current_sem").select().first().int_value
