690dfd2923ac731f9228042d3433fbb16ce34f0a
aditi741997 <aditi741997@gmail.com>
2016-03-04 13:57:55 +0530

    Minor changes

--- Server/parameters_8000.py
+++ Server/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$972ae7f968ff11f1$3ecfd37861e1b74fb7d7fb14fcb2b663236accc6"
+password="pbkdf2(1000,20,sha512)$847b1a3542862caa$5e0ebf43fae4401b739ad5d66cd8ffccdd9bbd6f"
